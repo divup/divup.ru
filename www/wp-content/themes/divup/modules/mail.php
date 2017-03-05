e1d459ec03223eaff5aa8b5c01de598be1f3c0ae
Alex Merser <divup.sup@gmail.com>
2017-03-05 20:34:22 +0300

    first commit

---
+++ www/wp-content/themes/divup/modules/mail.php
@@ -0,0 +1,9 @@
+<?php
+$recepient = 'divup.sup@gmail.com';
+$sitename = "divup.ru";
+$name = trim($_POST["fio"]);
+$contact = trim($_POST["connection"]);
+$g1 = trim($_POST["ch"]);
+$message = "Имя: $name \nКонтактные данные: $contact \nТип сайта: $g1";
+$pagetitle = "Заказ на звонок с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
